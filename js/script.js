b7ea47ffbe557507c5655ab1d095bc3d5edbe238
Duarte Silva <153696857+duasilva@users.noreply.github.com>
2025-11-06 20:35:53 -0100

    Updated countdown due to new delay

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const countdownEl = document.getElementById("countdown");
-const releaseDate = new Date("2026-05-26T00:00:00");
+const releaseDate = new Date("2026-11-19T00:00:00");
 
 function pad(num) {
-    return num.toString().padStart(2, "0");
+  return num.toString().padStart(2, "0");
 }
 
 function updateCountdown() {
-    const now = new Date();
-    const diff = releaseDate - now;
+  const now = new Date();
+  const diff = releaseDate - now;
 
-    if (diff <= 0) {
-        countdownEl.textContent = "GTA VI is out now!";
-        clearInterval(intervalId);
-        return;
-    }
+  if (diff <= 0) {
+    countdownEl.textContent = "GTA VI is out now!";
+    clearInterval(intervalId);
+    return;
+  }
 
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((diff / (1000 * 60)) % 60);
-    const seconds = Math.floor((diff / 1000) % 60);
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
 
-    countdownEl.textContent = `${days}D ${pad(hours)}H ${pad(minutes)}M ${pad(
-        seconds
-    )}S`;
+  countdownEl.textContent = `${days}D ${pad(hours)}H ${pad(minutes)}M ${pad(
+    seconds
+  )}S`;
 }
 
 const intervalId = setInterval(updateCountdown, 1000);
